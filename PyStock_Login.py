5d470dc11fa3ff76829f9da90edf3bdf893a2805
Geovani Debastiani <geovanidebastiani32@gmail.com>
2021-07-03 19:35:38 -0300

    Primeiras Telas e Configurações

---
+++ PyStock_Login.py
@@ -0,0 +1,75 @@
+import sys
+import mysql.connector
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from View.PY.FrmLogin import Ui_login
+
+banco = mysql.connector.connect(
+    host='localhost',
+    port='3307',
+    user='root',
+    passwd='',
+    database='banco_pystock'
+)
+
+cursor = banco.cursor()
+
+
+class Login(QMainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.ui = Ui_login()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(lambda: self.logar())
+
+    def logar(self):
+        cursor.execute("SELECT * FROM login")
+        logins = cursor.fetchall()
+
+        usuario = self.ui.lineEdit.text()
+        senha = self.ui.lineEdit_2.text()
+
+        print(f"Usuario inserido: {usuario}\n"
+              f"Senha inserida: {senha}")
+
+        for login in logins:
+
+            if usuario != login[0]:
+                self.ui.lineEdit.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
+                                               'border-bottom-color: rgb(255, 17, 49);color: rgb(0,0,0);padding-bottom: 8px;'
+                                               'border-radius: 0px;font: 10pt "Montserrat";')
+
+            if senha != login[1]:
+                self.ui.lineEdit_2.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
+                                               'border-bottom-color: rgb(255, 17, 49);color: rgb(0,0,0);padding-bottom: 8px;'
+                                               'border-radius: 0px;font: 10pt "Montserrat";')
+
+            if usuario == login[0] and senha == login[1]:
+
+                self.ui.lineEdit.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
+                                               'border-bottom-color: rgb(159, 63, 250);color: rgb(0,0,0);padding-bottom: 8px;'
+                                               'border-radius: 0px;font: 10pt "Montserrat";')
+
+                self.ui.lineEdit_2.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
+                                                 'border-bottom-color: rgb(159, 63, 250);color: rgb(0,0,0);padding-bottom: 8px;'
+                                                 'border-radius: 0px;font: 10pt "Montserrat";')
+                if login[2] == 'admin':
+                    print('Seu nivel de Login é: Admin')
+
+
+
+                if login[2] == 'colaborador':
+                    print('Seu nivel de Login é: Colaborador')
+                break
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Login()
+    window.show()
+    sys.exit(app.exec_())
---
+++ README.md
@@ -0,0 +1,10 @@
+Projeto desenvolvido por Geovani Debastiani
+
+linkedin: https://www.linkedin.com/in/geovani-debastiani/
+
+github: https://github.com/GeovaniTech
+
+O PyStock é um software com o intuito de fazer controle de estoques, 
+está sendo desenvolvido com python, qtDesigner e Mysql!
+
+Caso tenha sugestões de melhoria entre em contato :)
