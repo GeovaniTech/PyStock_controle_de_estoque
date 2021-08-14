3666d246303f838af3d1d0e9896ab0fc71dda0f8
Geovani Debastiani <geovanidebastiani32@gmail.com>
2021-08-14 14:07:30 -0300

    Configurando páginas do FrmAdmin, ajustando as tebelas e cliques dos botoes

--- PyStock - App.py
+++ PyStock - App.py
@@ -88,133 +88,116 @@ class FrmAdmin(QMainWindow):
         self.ui = Ui_FrmAdmin()
         self.ui.setupUi(self)
 
-        # Iniciando na página inicial
-        self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_home)
-
-        ## Clique dos botões
+        # Configurando páginas e os botões do menu
 
         # Home
         self.ui.btn_home.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_home))
 
-        # Cadastro de Colaboradores
+        # Colaboradores
+
         self.ui.btn_colaboradores.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_colaboradores))
-        self.ui.btn_cadastro.clicked.connect(self.CadastroColaborador)
-        
-        # Fornecedores
-        self.ui.btn_fornecedores.clicked.connect(
-            lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_fornecedores))
+        self.ui.btn_cadastrar_colaboradores.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_cadastro_colaboradores))
+        self.ui.btn_alterar_colaboradores.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.alterar_colaboradores))
+
+        # Tabela pg_colaboradores
+        self.ui.tabela_colaboradores.setColumnWidth(0, 260)
+        self.ui.tabela_colaboradores.setColumnWidth(1, 260)
+        self.ui.tabela_colaboradores.setColumnWidth(2, 260)
+
+        # Tabela alterar_colaboradores
+        self.ui.tabela_alterar_colaboradores.setColumnWidth(0, 330)
+        self.ui.tabela_alterar_colaboradores.setColumnWidth(1, 330)
+        self.ui.tabela_alterar_colaboradores.setColumnWidth(2, 330)
+
+        # Monitoramento
+        self.ui.btn_monitoramento.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.monitoramento))
+
+        # Tabela Monitoramento
+        self.ui.tabela_monitoramento.setColumnWidth(0, 156)
+        self.ui.tabela_monitoramento.setColumnWidth(1, 156)
+        self.ui.tabela_monitoramento.setColumnWidth(2, 156)
+        self.ui.tabela_monitoramento.setColumnWidth(3, 156)
+        self.ui.tabela_monitoramento.setColumnWidth(4, 156)
+
+        # Clientes
+        self.ui.btn_clientes.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_clientes))
+        self.ui.btn_cadastrar_clientes.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_cadastrar_clientes))
+        self.ui.btn_alterar_clientes.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_alterar_clientes))
+
+        # Tabela Clientes
+        self.ui.tabela_clientes.setColumnWidth(0, 192)
+        self.ui.tabela_clientes.setColumnWidth(1, 192)
+        self.ui.tabela_clientes.setColumnWidth(2, 192)
+        self.ui.tabela_clientes.setColumnWidth(3, 194)
+
+        # Tabela Cadastrar Clientes
+        self.ui.tabela_cadastrar_clientes.setColumnWidth(0, 247)
+        self.ui.tabela_cadastrar_clientes.setColumnWidth(1, 247)
+        self.ui.tabela_cadastrar_clientes.setColumnWidth(2, 247)
+        self.ui.tabela_cadastrar_clientes.setColumnWidth(3, 249)
+
+        # Tabela Alterar Clientes
+        self.ui.tabela_alterar_clientes.setColumnWidth(0, 247)
+        self.ui.tabela_alterar_clientes.setColumnWidth(1, 247)
+        self.ui.tabela_alterar_clientes.setColumnWidth(2, 247)
+        self.ui.tabela_alterar_clientes.setColumnWidth(3, 249)
 
         # Vendas
-        self.ui.btn_vendas.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_vendas))
+        self.ui.btn_Vendas.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_vendas))
+
+        # Fornecedores
+        self.ui.btn_fornecedores.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_fornecedores))
+        self.ui.btn_adicionar_forncedores.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_cadastrar_fornecedores))
+        self.ui.btn_editar_fornecedores.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_alterar_fornecedores))
+
+
+        # Tabela Fornecedores
+        self.ui.tabela_fornecedores.setColumnWidth(0, 257)
+        self.ui.tabela_fornecedores.setColumnWidth(1, 257)
+        self.ui.tabela_fornecedores.setColumnWidth(2, 257)
+
+        # Tabela Cadastrar Fornecedores
+        self.ui.tabela_cadastrar_fornecedores.setColumnWidth(0, 330)
+        self.ui.tabela_cadastrar_fornecedores.setColumnWidth(1, 330)
+        self.ui.tabela_cadastrar_fornecedores.setColumnWidth(2, 330)
+
+        # Tabela Alterar Fornecedores
+        self.ui.tabela_alterar_fornecedores.setColumnWidth(0, 330)
+        self.ui.tabela_alterar_fornecedores.setColumnWidth(1, 330)
+        self.ui.tabela_alterar_fornecedores.setColumnWidth(2, 330)
 
         # Produtos
         self.ui.btn_produtos.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_produtos))
-        self.ui.btn_alterar_produto.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_alterar_produtos))
         self.ui.btn_cadastrar_produto.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_cadastar_produtos))
+        self.ui.btn_alterar_produto.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_alterar_produtos))
 
-        # Voltar
-        self.ui.btn_voltar.clicked.connect(self.Voltar)
-
-        ## Ajustando Tabelas
-
-        # Ajustando Largura das Colunas da Tabela Vendas
-        self.ui.tabela_vendas.setColumnWidth(0, 45)
-        self.ui.tabela_vendas.setColumnWidth(1, 125)
-        self.ui.tabela_vendas.setColumnWidth(2, 250)
-        self.ui.tabela_vendas.setColumnWidth(3, 150)
-        self.ui.tabela_vendas.setColumnWidth(4, 45)
-        self.ui.tabela_vendas.setColumnWidth(5, 175)
-
-        # Ajustando Largura das Colunas da Tabela Produtos
-
-        self.ui.tabela_produto.setColumnWidth(0, 45)
-        self.ui.tabela_produto.setColumnWidth(1, 125)
+        # Tabela Produtos
+        self.ui.tabela_produto.setColumnWidth(0, 50)
+        self.ui.tabela_produto.setColumnWidth(1, 131)
         self.ui.tabela_produto.setColumnWidth(2, 250)
-        self.ui.tabela_produto.setColumnWidth(3, 150)
-        self.ui.tabela_produto.setColumnWidth(4, 45)
-        self.ui.tabela_produto.setColumnWidth(5, 175)
+        self.ui.tabela_produto.setColumnWidth(3, 131)
+        self.ui.tabela_produto.setColumnWidth(4, 75)
+        self.ui.tabela_produto.setColumnWidth(5, 155)
 
-        # Ajustando Largura das Colunas da Tabela Cadastrar Produtos
+        # Tabela Cadastrar Produtos
+        self.ui.tabela_cadastro.setColumnWidth(0, 50)
+        self.ui.tabela_cadastro.setColumnWidth(1, 165)
+        self.ui.tabela_cadastro.setColumnWidth(2, 300)
+        self.ui.tabela_cadastro.setColumnWidth(3, 165)
+        self.ui.tabela_cadastro.setColumnWidth(4, 75)
+        self.ui.tabela_cadastro.setColumnWidth(5, 250)
 
-        self.ui.tabela_cadastro.setColumnWidth(0, 45)
-        self.ui.tabela_cadastro.setColumnWidth(1, 125)
-        self.ui.tabela_cadastro.setColumnWidth(2, 350)
-        self.ui.tabela_cadastro.setColumnWidth(3, 150)
-        self.ui.tabela_cadastro.setColumnWidth(4, 45)
-        self.ui.tabela_cadastro.setColumnWidth(5, 300)
+        # Tabela Alterar Produtos
+        self.ui.tabela_alterar_produto.setColumnWidth(0, 50)
+        self.ui.tabela_alterar_produto.setColumnWidth(1, 165)
+        self.ui.tabela_alterar_produto.setColumnWidth(2, 300)
+        self.ui.tabela_alterar_produto.setColumnWidth(3, 165)
+        self.ui.tabela_alterar_produto.setColumnWidth(4, 75)
+        self.ui.tabela_alterar_produto.setColumnWidth(5, 250)
 
-        # Ajustando Largura das Colunas da Tabela Altearar Produtos
+        # Configurações
+        self.ui.btn_configs.clicked.connect(lambda: self.ui.Telas_do_menu.setCurrentWidget(self.ui.pg_configuracoes))
 
-        self.ui.tabela_alterar.setColumnWidth(0, 45)
-        self.ui.tabela_alterar.setColumnWidth(1, 125)
-        self.ui.tabela_alterar.setColumnWidth(2, 350)
-        self.ui.tabela_alterar.setColumnWidth(3, 150)
-        self.ui.tabela_alterar.setColumnWidth(4, 45)
-        self.ui.tabela_alterar.setColumnWidth(5, 300)
-
-        # Ajustando Largura das Colunas da Tabela Produtos
-
-        self.ui.tabela_fornecedores.setColumnWidth(0, 330)
-        self.ui.tabela_fornecedores.setColumnWidth(1, 330)
-        self.ui.tabela_fornecedores.setColumnWidth(2, 331)
-
-    def Voltar(self):
-        global window
-
-        window.close()
-        window = FrmLogin()
-        window.show()
-
-    def CadastroColaborador(self):
-        
-        nome = self.ui.line_nome
-        login = self.ui.line_login
-        senha = self.ui.line_senha
-
-        if nome.text() == "":
-            nome.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
-                                            'border-bottom-color: rgb(255, 17, 49);color: rgb(0,0,0);padding-bottom: 8px;'
-                                            'border-radius: 0px;font: 10pt "Montserrat";')
-        if login.text() == "":
-            login.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
-                                            'border-bottom-color: rgb(255, 17, 49);color: rgb(0,0,0);padding-bottom: 8px;'
-                                            'border-radius: 0px;font: 10pt "Montserrat";')
-        
-        if senha.text() == "":
-            senha.setStyleSheet('background-color: rgba(0, 0 , 0, 0);border: 2px solid rgba(0,0,0,0);'
-                                                'border-bottom-color: rgb(255, 17, 49);color: rgb(0,0,0);padding-bottom: 8px;'
-                                                'border-radius: 0px;font: 10pt "Montserrat";')
-        
-        elif self.ui.radio_colaborador.isChecked() == False and self.ui.radio_admin.isChecked() == False:
-            self.popup()
-        
-        elif nome.text() != "" and login.text() != "" and senha.text() != "":
-            cursor.execute('SELECT * FROM login')
-            banco_login = cursor.fetchall()
-
-            for user in banco_login:
-
-                if user[0] != login.text():
-
-                    if self.ui.radio_admin.isChecked() == True:
-                        comando_SQL = 'INSERT INTO login (usuario, senha, nivel, nome) VALUES (%s,%s,%s,%s)'
-                        dados = (f"{login.text()}", f"{senha.text()}", "admin", f"{nome.text()}")
-                        cursor.execute(comando_SQL, dados)
-                        banco.commit()
-
-                    if self.ui.radio_colaborador.isChecked() == True:
-                        comando_SQL = 'INSERT INTO login (usuario, senha, nivel, nome) VALUES (%s,%s,%s,%s)'
-                        dados = (f"{login.text()}", f"{senha.text()}", "colaborador", f"{nome.text()}")
-                        cursor.execute(comando_SQL, dados)
-                        banco.commit()
-
-    def popup(self):
-        msg = QMessageBox()
-        msg.setWindowTitle("Erro - Cadastro de Colaboradores")
-        msg.setText('Selecione um Nível de Usuário!')
-
-        x = msg.exec_()
-            
 
 class FrmColaborador(QMainWindow):
 
